ec29294f63b32dd92a3458d7556ebd9a03e90215
James Whale <43613409+WhaleJ84@users.noreply.github.com>
2020-06-03 18:57:51 +0100

    made first quick attempt at fixing Matts code

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -5,34 +5,34 @@ sudo apt-get update && sudo apt-get upgrade -y
 echo "Setting up aliases..."
 mv .bash_aliases ~
 echo "Complete"
-echo "Installing Vim..."
-sudo apt install -y vim &>/dev/null && echo 'Complete' || echo 'Error'
-echo "Installing Java JDK & JRE"
-sudo apt install -y openjdk-8-jre-headless &>/dev/null && echo 'Complete' || echo 'Error'
-echo "Installing Atom..."
-sudo snap install atom --classic
-echo "Installing Chromium..."
-sudo snap install chromium
-echo "Installing Deja Dup Backup Tool..."
-sudo snap install deja-dup --classic
-echo "Installing Discord..."
-sudo snap install discord
-echo "Installing Draw IO..."
-sudo snap install drawio
-echo "Installing Eclipse..."
-sudo snap install eclipse --classic
-echo "Installing Gnome Tweaks..."
-sudo apt install -y gnome-tweak-tool &>/dev/null && echo 'Complete' || echo 'Error'
-echo "Installing Libre Office..."
-sudo snap install libreoffice
-echo "Installing Postman..."
-sudo snap install postman
-echo "Installing Rhythmbox..."
-sudo apt install -y rhythmbox &>/dev/null && echo 'Complete' || echo 'Error'
-echo "Installing Spotify..."
-sudo snap install spotify
-echo "Installing VLC Player..."
-sudo snap install vlc
+
+aptinstall(){
+  echo "Installing $1..."
+  sudo apt install -y $2 &>/dev/null && echo "Installed $1" || echo "Error installing $1"
+}
+
+snapinstall(){
+  echo "Installing $1..."
+  sudo snap install $2
+}
+
+aptinstall Vim vim
+aptinstall 'Java JDK & JRE' openjdk-8-jre-headless
+aptinstall 'Gnome Tweaks' gnome-tweak-tool
+aptinstall Rhythmbox rhythmbox
+
+snapinstall Atom 'atom --classic'
+snapinstall Chromium chromium
+snapinstall 'Deja Dup Backup Tool' 'deja-dup --classic'
+snapinstall Discord discord
+snapinstall 'Draw IO' drawio
+snapinstall 'Eclipse' 'eclipse --classic'
+snapinstall 'Gnome Tweaks' gnome-tweak-tool
+snapinstall 'Libre Office' libreoffice
+snapinstall Postman postman
+snapinstall Spotify spotify
+snapinstall 'VLC Player' vlc
+
 echo "Final update check..."
 sudo apt-get install && sudo apt-get update -y
 sudo snap refresh
