cfda79a480b84f03acbfe5ccc07e4049beb7fde5
Matt Dear <mrmdear@gmail.com>
2020-07-09 18:38:28 +0100

    Added missing full stops

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -30,9 +30,9 @@ apminstall(){
 }
 
 # Setting up Atom installer
-echo -e "Retrieving Atom GPG Key"
+echo -e "Retrieving Atom GPG Key..."
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add - 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
-echo -e "Adding Atom To Apt Sources"
+echo -e "Adding Atom To Apt Sources..."
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list' 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 #sudo apt-get update
 
@@ -40,9 +40,9 @@ sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ 
 aptinstall Curl curl
 
 # Setting up Spotify installer
-echo -e "Retrieving Spotify GPG Key"
+echo -e "Retrieving Spotify GPG Key..."
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
-echo -e "Adding Spotify To Apt Sources"
+echo -e "Adding Spotify To Apt Sources..."
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 #sudo apt-get update
 
