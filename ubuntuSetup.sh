21865edeb3246dcf3a6cd09479d813ef63cd44fe
Matt Dear <mrmdear@gmail.com>
2020-07-09 18:30:47 +0100

    Update all message handling for commands.

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -20,20 +20,20 @@ aptinstall(){
 # Custom snap install function
 snapinstall(){
   echo -e "Installing $1... \c"
-  snap install $2 1>/dev/null && echo -e "Complete" || echo -e "Error"
+  snap install $2 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 }
 
 # Custom apm install function
 apminstall(){
   echo -e "Installing $1... \c"
-  apm install $2 1>/dev/null && echo -e "Complete" || echo -e "Error"
+  apm install $2 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 }
 
 # Setting up Atom installer
 echo -e "Retrieving Atom GPG Key"
-wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add - 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 echo -e "Adding Atom To Apt Sources"
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list' 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 #sudo apt-get update
 
 # Installing cURL
@@ -41,9 +41,9 @@ aptinstall Curl curl
 
 # Setting up Spotify installer
 echo -e "Retrieving Spotify GPG Key"
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 echo -e "Adding Spotify To Apt Sources"
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 #sudo apt-get update
 
 # Update start
