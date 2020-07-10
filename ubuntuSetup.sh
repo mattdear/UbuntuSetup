e4f8c5021b97a4ab37b9c7acc236eea64bc11476
Matt Dear <mrmdear@gmail.com>
2020-07-10 09:31:19 +0100

    Major length test.

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -24,57 +24,57 @@ snapinstall(){
 }
 
 # Setting up Atom installer
-echo "Retrieving Atom GPG key................................................ \c"
+echo "Retrieving Atom GPG key ............................................... \c"
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add - 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
-echo "Adding Atom to apt sources............................................. \c"
+echo "Adding Atom to apt sources ............................................ \c"
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list' 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 
 # Installing cURL
-aptinstall Curl........................................................ curl
+aptinstall Curl ....................................................... curl
 
 # Setting up Spotify installer
-echo "Retrieving Spotify GPG key............................................. \c"
+echo "Retrieving Spotify GPG key ............................................ \c"
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
-echo "Adding Spotify to apt sources.......................................... \c"
+echo "Adding Spotify to apt sources ......................................... \c"
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 
 # Update start
-echo "Updating............................................................... \c"
+echo "Updating .............................................................. \c"
 apt update 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 
 # Upgrade start
-echo "Upgrading.............................................................. \c"
+echo "Upgrading ............................................................. \c"
 apt upgrade -y 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 
 # Configure aliases
-echo "Configuring Aliases.................................................... \c"
+echo "Configuring Aliases ................................................... \c"
 mv .bash_aliases $home
 chown $user:$user $home/.bash_aliases
 echo "Complete"
 
 if [ $(ls '/sys/class/power_supply/' 2>/dev/null) ] ; then
   # LAPTOP
-  snapinstall Deja Dup 'deja-dup --classic'
+  snapinstall Deja Dup .................................................. 'deja-dup --classic'
 else
   # DESKTOP
-  aptinstall Boxes..................................................... gnome-boxes
+  aptinstall Boxes ...................................................... gnome-boxes
 fi
 
 # Apt installs
-aptinstall Vim......................................................... vim
-aptinstall 'Java JDK & JRE' openjdk-8-jre-headless
-aptinstall 'Gnome Tweaks' gnome-tweak-tool
-aptinstall Maven maven
-aptinstall 'Hunspell en-GB' hunspell-en-gb
-aptinstall Atom atom
-aptinstall Spotify spotify-client
+aptinstall Vim ........................................................ vim
+aptinstall 'Java JDK & JRE .............................................' openjdk-8-jre-headless
+aptinstall 'Gnome Tweaks ...............................................' gnome-tweak-tool
+aptinstall Maven ...................................................... maven
+aptinstall 'Hunspell en-GB .............................................' hunspell-en-gb
+aptinstall Atom ....................................................... atom
+aptinstall Spotify .................................................... spotify-client
 
 # Snap installs
-snapinstall Postman postman
-snapinstall DrawIO drawio
-snapinstall Netbeans 'netbeans --classic'
-snapinstall 'Libre Office' libreoffice
-snapinstall Discord discord
+snapinstall Postman ................................................... postman
+snapinstall DrawIO .................................................... drawio
+snapinstall Netbeans .................................................. 'netbeans --classic'
+snapinstall 'Libre Office ..............................................' libreoffice
+snapinstall Discord ................................................... discord
 
 # Script end
 echo "############################# UbuntuSetup Complete #############################"
