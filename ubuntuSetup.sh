de549de9a1080e74cc57511c6d3c6ae4a3dbe69b
Matt Dear <mrmdear@gmail.com>
2020-07-23 12:50:36 +0100

    Made tex live manual install

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -59,7 +59,6 @@ aptinstall 'Vim ........................................................' vim
 aptinstall 'Java JDK & JRE .............................................' openjdk-8-jre-headless
 aptinstall 'Gnome Tweaks ...............................................' gnome-tweak-tool
 aptinstall 'Maven ......................................................' maven
-aptinstall 'TexLive ....................................................' texlive
 aptinstall 'LaTeXmk ....................................................' latexmk
 
 # Snap installs
@@ -86,6 +85,7 @@ promptcontinue "Install Code Spell Checker .....................................
 echo " "
 echo "                                Manual installs                                 "
 echo "                      Press [Enter] after each to continue                      "
+promptcontinue "Tex Live .............................................................. \c"
 promptcontinue "Virtual Box ........................................................... \c"
 promptcontinue "Zotero ................................................................ \c"
 
